8dfc517521342a4eac27f4a582deb0f610e88253
Arush-creator9 <arushkumar.809@gmail.com>
2025-09-18 10:47:00 +0530

    Add files via upload

---
+++ exercise.py
@@ -0,0 +1,13 @@
+print("=== PAYROLL CALCULATOR ===")
+name = input("enter employee name:- ")
+hours = float(input("enter hours worked: "))
+rate = float(input("enter hourly pay rate: $"))
+if hours > 40:
+    print("overtime = hours-40")
+else:
+    print("overtime = hours+40")
+
+print("=== PAYROLL SUMMARY ===")
+print(f"Employee: {name}")
+print(f"Hours: {hours}")
+print(f"Rate: {rate}")
